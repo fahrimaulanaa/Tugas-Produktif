4a1fecd06db0c375de8719750c0a780feb87039f
Fahri Maulana <88439222+fahrimaulanaa@users.noreply.github.com>
2022-08-02 12:55:47 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function sapa(){
+    alert("Hallo everybody");
+}
+
+function sapalagi(){
+    alert("Halo lagi");
+}
+
+function sapanama(){
+    const nama = prompt("Masukkan nama anda");
+    alert("Halo " + nama);
+}
